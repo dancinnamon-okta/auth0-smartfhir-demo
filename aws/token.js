a6d42ca2136ada3fb0277f5e8c073e496a5c7c18
Dan Cinnamon <dan.cinnamon@okta.com>
2021-07-15 15:39:19 -0500

    Initial release

---
+++ aws/token.js
@@ -0,0 +1,19 @@
+'use strict';
+const tokenLib = require('../lib/token')
+
+//Token proxy - AWS implementation.
+//See the token library for full documentation.
+module.exports.tokenHandler = async (event, context) => {
+	var handlerResponse = await tokenLib.tokenHandler(event.body, event.headers)
+
+	return {
+		statusCode: handlerResponse.statusCode,
+		body: JSON.stringify(handlerResponse.body),
+		headers: {
+			"Cache-Control": "no-store",
+			"Pragma": "no-cache",
+			'Access-Control-Allow-Origin': '*', // CORS
+			'Access-Control-Allow-Credentials': true // Required for cookies, authorization headers with HTTPS
+		}
+	}
+}
